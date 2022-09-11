f8fc59a3bf143fdfc541beb96c4ec1564c528b6a
lex <rt@desk-rt>
2022-09-11 19:25:40 +0300

    next commit

---
+++ lesson2.py
@@ -0,0 +1,8 @@
+others_dict = {
+    "city": "Москва",
+    "temperature": "20"
+}
+print(others_dict["city"])
+others_dict["temperature"] = "15"
+print(others_dict["temperature"])
+print(others_dict)
